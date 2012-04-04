62a4789bbeb637a32955d62a153b8132067fcf3e
Adam Krebs <amk528@cs.nyu.edu>
2012-04-04 10:04:14 -0700

    move backend and other PHP to main farmtab site / repo

---
+++ includes/secrets.php
@@ -0,0 +1,16 @@
+<?
+	class secrets {
+
+		const TOKEN_SECRET = 'I<3CSAs!';
+		
+		public static function DB_INFO() {
+			return array(
+				'host' => 'mysql.farmtab.com',
+				'user' => 'farmtab',
+				'pass' => '7bdpHZqb',
+				'db' => 'farmtab'
+			);
+		}
+	}
+
+?>
