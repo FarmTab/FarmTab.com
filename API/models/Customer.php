8808d2e65da056cd20db9f0b0aaf767ed21a3a0a
Adam Krebs <amk528@cs.nyu.edu>
2012-06-13 16:30:59 -0700

    move Customer to php-activerecord instead of home-baked ORM

--- API/models/Customer.php
+++ API/models/Customer.php
@@ -1,49 +1,25 @@
 <?php
 
-  class Model {
+  class Customer extends ActiveRecord\Model {
 
+    static $validates_presence_of = array(
+      array('name'), array('pin'), array('email')
+    );
 
-    public function __construct($json) {
-      $model = json_decode($json);
-    }
+    static $validates_length_of = array(
+      array('pin', 'within' => array(4,6),
+        'too_short' => "PIN must be at least 4 characters",
+        'too_long'  => "PIN must cannot be more than 6 characters"
+      )
+    );
 
-    public function fetch() {
-      $data = $db->row(
-        'table'     => self::$tablename,
-        'condition' => "id={$this->id}"
-      );
-    }
+    static $validates_numericality_of = array(
+      array('pin', 'only_integer' => true)
+    );
 
-    public function isNew() {
-      return !isset($this->id);
-    }
-
-    public function save() {
-
-      $data = array_map('mysql_real_escape_string', $this);
-
-      if (isNew()) {
-        $this->id = $db->insert($this->tablename, $data);
-      } else {
-         $db->update($this->tablename, $data, "id={$this->id}");
-      }
-    }
-
-    public function _validate(options) {
-      $error = $this->validate(options);
-    }
-
-  }
-
-
-  class Customer extends Model {
-
-    const tablename = 'Customers';
-  
-    public $id;
-    public $name;
-    public $email;
-    private $pin
+    static $validates_uniqueness_of = array(
+      array('email')
+    );
 
 
     public function __construct($json) {
@@ -53,37 +29,6 @@
 
       $id = $model['id'];
     }
-
-
-    public function validate($model) {
-
-      $name  = $model['name'];
-      $pin   = $model['pin'];
-      $email = $model['email'];
-
-      // validate name
-      if (!isset($name))
-        failure("name not set");
-      if (1 <= strlen($name))
-        failure("name too short.");
-
-      // validate pin
-      if (!isset($pin))
-        failure("pin not set");
-      if (6 > strlen($pin))
-        failure("PIN too long. cannot be more than 6 characters");
-      if (4 <= strlen($pin))
-        failure("PIN too short. must be at least 4 characters");
-
-      // validate email
-      if (!isset($email))
-        failure("email not set");
-      if (!filter_var($email, FILTER_VALIDATE_EMAIL))
-        failure("invalid email");
-
-       return true;
-    }
-    
 
   }
 
