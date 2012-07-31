fb7313578b5514810ae702133c51289d8b6cde69
Adam Krebs <amk528@cs.nyu.edu>
2012-07-30 20:38:19 -0700

    transition API backend to rails-API

---
+++ API/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+API::Application.config.secret_token = '6422028f9f35f178ba04038f68ba767295801dbce414ff02503545a139874347087da2e71c2c72d8bdb5007f6346a38e6abfdebcf5ade0f943085014e038fecd'
